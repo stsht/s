d03f093de2bca8da668542b6093a13f5a40c455b
stsht <growing_tux.7i@icloud.com>
2026-07-02 15:33:14 +0700

    Use shared delivery preview message

--- src/pages/l/linkMessage.js
+++ src/pages/l/linkMessage.js
@@ -1,25 +1,12 @@
 import { cleanLinkText } from './linkHelpers.js';
+import { buildDeliveryMessageIg } from '../../features/deliveries/deliveryMessages.js';
 
 export function buildPreviewMessage(title, clientName, info) {
-  const link = info.shortLink || info.directUrl;
-  const t = cleanLinkText(title);
-  const c = cleanLinkText(clientName);
-  const namePart = t ? `${t} ${c}` : c;
-  return `Dear ${namePart},
-
-With sincere appreciation, your private StarShots delivery page has been prepared for your kind attention.
-
-You may access your Delivery Page and Invoice through the details below:
-
-• Link: ${link}
-• Password: ${info.pass}
-
-Should you wish to use a different password, please feel free to let us know and we will be pleased to update it for you.
-
-Kindly keep this link for your delivery updates. Your final files will be made available through the same page once they are ready.
-
-Thank you once again for allowing StarShots ID to be part of your special moment.
-
-Warm Regards,
-StarShots ID`;
+  return buildDeliveryMessageIg({
+    title: cleanLinkText(title),
+    clientName: cleanLinkText(clientName),
+    link: info.shortLink || info.directUrl,
+    password: info.pass,
+    previewOnly: true,
+  });
 }
